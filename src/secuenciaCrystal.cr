7dc068ea0191daf0cf4c007b10b55950643c209f
peg <emanuelpeg@yahoo.com.ar>
2018-01-07 21:47:57 -0300

    primer commit

---
+++ src/secuenciaCrystal.cr
@@ -0,0 +1,6 @@
+require "./secuenciaCrystal/*"
+
+# TODO: Write documentation for `SecuenciaCrystal`
+module SecuenciaCrystal
+  # TODO: Put your code here
+end
---
+++ src/secuenciaCrystal/version.cr
@@ -0,0 +1,3 @@
+module SecuenciaCrystal
+  VERSION = "0.1.0"
+end
